5c1017baff1fe005e4619c4aad05332b79aeea48
Daniel Neto <daniel@danielneto.dev.br>
2022-10-01 15:13:29 -0300

    implements the dashboard layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'wk-technology-web';
+  public showDropdownMenu = false
+
+  public toggleDropdownMenu(): void {
+    this.showDropdownMenu = !this.showDropdownMenu
+  }
 }
